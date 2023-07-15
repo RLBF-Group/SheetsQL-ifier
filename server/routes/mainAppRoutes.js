e2159fa25eb0d3b063bf7166205f8dfb4b5f2f15
Haoyu <liu_haoyu@berkeley.edu>
2023-07-15 10:36:46 -0700

    setting up api, linking to sql, creating template for table

---
+++ server/routes/mainAppRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+//REQUIRE CONTROLLERS
+const mainAppController = require('../controllers/mainAppController');
+
+router.get('/', (req, res) => {
+    return res.status(200).json('some object');
+})
+
+
+
+//export router
+module.exports = router;
+
