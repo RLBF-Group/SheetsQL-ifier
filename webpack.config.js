774063a2ba3cdf880103fd410898f3750b936f75
Jacob <135070461+JacobAMelendez@users.noreply.github.com>
2023-07-14 20:03:17 -0500

    setting up boiler plate

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+var webpack = require('webpack');
+const HTMLWebPlugin = require('html-web-plugin')
+
+module-exports = {
+  entry: "./client/index.js",
+  output: "8080",
+  devServer: ,
+  proxy: ,
+  
+}
