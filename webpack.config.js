dc154a18116fcfa216b7ffc0ac5d26e13948e6c3
peterzepf <ptrgzpf@gmail.com>
2023-07-15 13:11:42 -0400

    working with api

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,9 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
         },
       },
       {
