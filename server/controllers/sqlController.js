f7ee30f6d9a144785aa7f2ef62788f327a1c94b3
edwinlny <ed.leong89@gmail.com>
2023-07-17 15:49:59 -0400

    fixed createTable. waiting for tableName res.locals

--- server/controllers/sqlController.js
+++ server/controllers/sqlController.js
@@ -6,139 +6,76 @@ const sqlController = {};
 const fsCallback = require('fs');
 const path = require('path');
 
-const tableName = 'peopleInJapan';
+
 let colNames = [];
-primaryKey = 'id';
 
-sqlController.createTable = (req, res, next) => {
-  // const {primaryKey} = req.body
-  //get item from each row in that position
-  const matrixTranspose = (matrix) => {
-    const transposed = [];
+sqlController.createTable = async (req, res, next) => {
+  const {primaryKey} = req.body
+  
+  //get sheet from res.locals
+  const sheet = res.locals.data;
+  const tableName = 'peopleInAfrica'; //pending input from Peter
+  console.log('sheet:', sheet);
 
-    for (let i = 0; i < matrix[0].length; i++) {
-      const row = [];
-      for (let j = 0; j < matrix.length; j++) {
-        row.push(matrix[i][j]);
-      }
-      transposed.push(row);
-    }
-    return transposed;
-  };
-  // [
-  //     ["id", "name"],
-  //     ["1", "q"],
-  //     ["2", "w"]
-  // ]
-  fsCallback.readFile(
-    path.resolve(__dirname, '../models/data.json'),
-    'UTF-8',
-    (err, data) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+  // assign primary key
+  if(!primaryKey) primaryKey = sheets[0][0];
+ 
 
-      // return next(createErr({
-      //     method: 'getCharacters',
-      //     type: 'reading file',
-      //     err,
-      //   }));
-      //console.log(data)
-      const sheet = JSON.parse(data).data.values;
-      console.log('sheet:', sheet);
-      const reversedSheet = matrixTranspose(sheet);
-      // [
-      //     ["id", "1", "2"],
-      //     ["name", "q", "w"]
-      //
-      // ]
+  //get length of rows and columns
+  const numOfRows = sheet.length;
+  const numOfCols = sheet[0].length;
+ 
 
-      console.log('reveredSheet:', sheet);
-      for (let col = 0; col < reversedSheet.length; col++) {
-        if (reversedSheet[col][0] === primaryKey) {
-          if (
-            new Set(reversedSheet[col][0].slice(1)).size !==
-            reversedSheet[col][0].slice(1).length
-          ) {
-            console.log('primary key not unique');
-            return;
-          }
-        }
-      }
-      // res.locals.characters = parsedData.results;
-      // return next();
+  // make $1, $2, $3... our conditional checks if we reached the end, if so we no longer need a comma
+  let value = '';
+  for (let i = 0; i < numOfCols; i++) {
+    if (i === numOfCols - 1) value += `$${i + 1}`;
+    else value += `$${i + 1},`;
+  }
 
-      //create an empty matrix based on our data.json
-      const numOfRows = sheet.length;
-      const numOfCols = sheet[0].length;
-      //   let matrix = new Array(numOfRows);
+  console.log('value: ', value);
 
-      //   for (let i = 0; i < numOfRows; i++) {
-      //     matrix[i] = new Array(numOfCols);
-      //   }
+  //grabs our column names
+  colNames = sheet[0];
+ 
+  //insert values into respective columns
+  const text2 = `INSERT INTO ${tableName} (${colNames.join(',')}) VALUES (${value})`;
+  console.log('text2: ', text2);
 
-      //   //assign each cell value into it's respective matrix index
-      //   sheet.forEach((col, i) => {
-      //     //colNames.push(col[0])
-      //     col.forEach((cell, j) => {
-      //       matrix[j][i] = cell;
-      //     });
-      //   });
-      // console.log(matrix)
-      // transform col array to two array
+  //assigns the type of values (varchar) that can be input into our columns
+  let subText = '';
+  for (let i = 0; i < colNames.length; i++) {
+    subText += `${colNames[i].replace(/\s/g, '')} varchar,`;
+  }
+  // console.log('subText:', subText)
 
-      // make $1, $2, $3... our conditional checks if we reached the end, if so we no longer need a comma
-      let value = '';
-      for (let i = 0; i < numOfCols; i++) {
-        if (i === numOfCols - 1) value += `$${i + 1}`;
-        else value += `$${i + 1},`;
-      }
-
-      console.log('value: ', value);
-
-      //grabs our column names
-      colNames = sheet[0];
-      // console.log("colNames: ", colNames)
-
-      //insert values into respective columns
-      const text2 = `INSERT INTO ${tableName} (${colNames.join(
-        ','
-      )}) VALUES (${value})`;
-      console.log('text2: ', text2);
-
-      //assigns the type of values (varchar) that can be input into our columns
-      let subText = '';
-      for (let i = 0; i < colNames.length; i++) {
-        subText += `${colNames[i].replace(/\s/g, '')} varchar,`;
-      }
-      // console.log('subText:', subText)
-
-      //our query text if table does not exist yet
-      const text1 = `CREATE TABLE IF NOT EXISTS ${tableName} (
+  //our query text if table does not exist yet
+  const text1 = `CREATE TABLE IF NOT EXISTS ${tableName} (
                   ${subText}
                   PRIMARY KEY (${primaryKey})
                 )`;
 
-      console.log('text1: ', text1);
+  console.log('text1: ', text1);
 
-      //using async await, we push our data using a for loop
+  //using async await, we push our data using a for loop
 
-      async function queryDB() {
-        try {
-          const createTable = await db.query(text1);
-          //may need to slice out first element to get rid of our column name, leaving it as i+1 for now
-          for (let i = 1; i < sheet.length; i++) {
-            await db.query(text2, sheet[i]);
-          }
-        } catch (err) {
-          console.log(`invalid entry :${err}`);
-        }
+  async function queryDB() {
+    try {
+      const createTable = await db.query(text1);
+      //may need to slice out first element to get rid of our column name, leaving it as i+1 for now
+      for (let i = 1; i < sheet.length; i++) {
+        await db.query(text2, sheet[i]);
       }
-      queryDB();
-    }
-  );
+    } catch (err) {
+      console.log(`invalid entry :${err}`);
+      return next({
+        log:`sqlController.createTable: Error: ${err}`,
+        message: {err: 'Error in your controller. Check server logs for details'}
+      });
+    } 
+  }
+  await queryDB();
+   return next();
 };
-sqlController.createTable();
 
 module.exports = sqlController;
--- server/routes/mainAppRoutes.js
+++ server/routes/mainAppRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-//REQUIRE CONTROLLERS
-const mainAppController = require('../controllers/mainAppController');
+// //REQUIRE CONTROLLERS
+// const mainAppController = require('../controllers/mainAppController');
 
-router.get('/', mainAppController.execTerminal, (req, res) => {
-    return res.status(200).send(res.locals.database);
-})
+// router.get('/', mainAppController.execTerminal, (req, res) => {
+//     return res.status(200).send(res.locals.database);
+// })
 
-//export router
-module.exports = router;
+// //export router
+// module.exports = router;
