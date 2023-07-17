594c0d7c233cc8195b7e8bba7ad51739d29ce112
peterzepf <ptrgzpf@gmail.com>
2023-07-17 16:22:04 -0400

    field mask working

--- server/controllers/gSheetsController.js
+++ server/controllers/gSheetsController.js
@@ -3,6 +3,9 @@ const sheets = google.sheets('v4');
 
 const gSheetsController = {};
 
+const fieldMask =
+  'properties(title),sheets.properties(title),sheets.data(rowData(values.formattedValue))';
+
 gSheetsController.testGetData = async (req, res, next) => {
   console.log('attempting API call');
   try {
@@ -10,6 +13,8 @@ gSheetsController.testGetData = async (req, res, next) => {
       spreadsheetId: '1zp4arRs66urJqI_iUOM4QcstUuW9OXq3blwgWn8TEvQ',
       // range: 'Sheet1!A1:B6',
       includeGridData: true,
+      fields:
+        'properties(title),sheets.properties(title),sheets.data(rowData(values.formattedValue))',
       auth: res.locals.auth,
     });
     console.log('collected sheetData');
@@ -18,7 +23,34 @@ gSheetsController.testGetData = async (req, res, next) => {
     return next();
   } catch (err) {
     // big error
-    console.log('here?');
+    console.log(err);
+    return next(err);
+  }
+};
+
+gSheetsController.getDataFromRange = async (req, res, next) => {
+  const { dataRange: range, googleSheetsUrl } = req.body;
+  const spreadsheetId = '';
+  const auth = res.locals.auth;
+  try {
+    let data;
+    if (range) {
+      const request = {
+        spreadsheetId,
+        range,
+        auth,
+      };
+      data = sheets.spreadsheets.values.get(request);
+    } else {
+      const request = {
+        spreadsheetId,
+        fields: fieldMask,
+        auth,
+      };
+    }
+    res.locals.data = data;
+    return next();
+  } catch (err) {
     return next(err);
   }
 };
