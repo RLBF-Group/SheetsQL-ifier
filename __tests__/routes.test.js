2fae8d006e8b5797995432665bb415088636a586
gschussler <grantschussler@gmail.com>
2023-07-19 14:55:03 -0500

    made progress testing endpoints

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,8 +1,31 @@
 // import routers/endpoints from files throughout the project to test them here
-// also need supertest
-const supertest = require('supertest');
-const server = require('../server/server.js');
+// supertest will be a required for http request testing
+const request = require('supertest');
+const app = require('../server/server');
 
-describe("server on startup", () => {
-  
+// *** REFER TO `server.js` TO MAKE SURE Application Default Credentials (ADC) ARE AUTHENTICATED ***
+
+// Google API Authentication is essential to server-side operations in the application, but using Jest to mock that authentication here seemed to be unnecessary and inefficient. Google API docs will be of more help
+
+
+describe("server endpoints", () => {
+  let server;
+
+  beforeAll(() => {
+    server = app.listen(5555); //start server at PORT before tests
+  })
+
+  afterAll((done) => {
+    if(server) {
+    server.close(done) //close server after tests run
+    } else {
+      done();
+    }
+  });
+
+  it('should start and listen at proper port', async () => {
+    const res = await request(app).get('/');
+    expect(res.status).toBe(404);
+    expect(res.text).toBe('Not Found');
+  })
 })
