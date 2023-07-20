1448b44c7054f46ad1232384c1105c8ba2e5e0d6
tristanyukio <yukiokrause@gmail.com>
2023-07-20 18:39:39 -0400

    updated 6:39

--- client/components/Home.js
+++ client/components/Home.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-        <Link to="/form" className="linkToForm"><button>Original SheetsQL-ifier</button></Link>
-        <Link to="/reverse" className="linkToReverse"><button>Reversifier</button></Link>
+    <div className="button-home">
+        <Link to="/form" className="linkToForm"><button>SheetsQL-ifier</button></Link>
+        <Link to="/reverse" className="linkToReverse"><button className='rev-button'>Reversifier</button></Link>
     </div>
   )
 }
