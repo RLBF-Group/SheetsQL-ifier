b1c10d954a8dfdc8ad6bf03f5b41cf9c5a2fd262
gschussler <grantschussler@gmail.com>
2023-07-20 14:26:59 -0500

    authController update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -65,7 +65,7 @@ authController.getOAuthClient = (req, res, next) => {
 			const oauth2Client = new google.auth.OAuth2(
 				process.env.GOOGLE_CLIENT_ID,
 				process.env.GOOGLE_CLIENT_SECRET,
-				'http://localhost:8080/api/authentication/callback'
+				'http://localhost:8080/callback'
 			);
 			store.push(oauth2Client);
 		}
